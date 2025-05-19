24510798269d75b495c3fb1fc3f48edb4f81853e
w757 <weronika.rzeszutek27@gmail.com>
2025-05-19 10:29:14 -0400

    project cleaning

--- Proxy/models.py
+++ Proxy/models.py
@@ -1,9 +1,6 @@
-from flask_sqlalchemy import SQLAlchemy
+from extensions import db
 from flask_login import UserMixin
 from werkzeug.security import generate_password_hash, check_password_hash
-
-
-db = SQLAlchemy()
 
 
 class User(db.Model, UserMixin):
