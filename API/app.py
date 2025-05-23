e61dee9cd2de1dd1f6033361349032f98dfd565e
w757 <weronika.rzeszutek27@gmail.com>
2025-05-23 12:37:50 -0400

    project cleaning

--- API/app.py
+++ API/app.py
@@ -332,4 +332,5 @@ with open('static/swagger.json', 'w') as f:
     json.dump(swagger_json, f, indent=4)
 
 if __name__ == '__main__':
-    app.run(port=8080, debug=True)
+    app.run(host='0.0.0.0', port=8080, debug=True)
+
